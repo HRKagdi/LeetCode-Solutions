4d6e2a3cb7e3699e4399b5298ea56f1b5d4be827
HRKagdi <53858937+HRKagdi@users.noreply.github.com>
2021-09-01 22:56:11 +0530

    Create PowerOfThree.cpp

---
+++ PowerOfThree.cpp
@@ -0,0 +1,12 @@
+//https://leetcode.com/problems/power-of-three/
+//326. Power of Three
+
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n==0) return false;
+        if(n==1) return true;
+        if(n%3!=0) return false;
+        return isPowerOfThree(n/3);
+    }
+};
